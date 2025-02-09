de375d65224d654448aeb7491f898e7ecd208e69
Saikrishna65 <sai.mangina789@gmail.com>
2025-02-09 10:42:18 +0530

    changed webhook controller

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -4,45 +4,51 @@ import User from "../models/User.js";
 export const clerkWebhooks = async (req, res) => {
   try {
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-    await whook.verify(JSON.stringify(req.body), {
+
+    // Use `req.rawBody` instead of `JSON.stringify(req.body)`
+    await whook.verify(req.rawBody, {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
     });
 
     const { data, type } = req.body;
-    switch (key) {
+
+    switch (
+      type // Fix: Use `type` instead of `key`
+    ) {
       case "user.created": {
         const userData = {
           _id: data.id,
-          email: data.email_address[0].email_address,
-          name: data.first_name + " " + data.last_name,
+          email: data.email_addresses[0].email_address, // Fix: Correct field name
+          name: `${data.first_name} ${data.last_name}`,
           imageUrl: data.image_url,
         };
         await User.create(userData);
-        res.json({});
+        res.json({ success: true });
         break;
       }
       case "user.updated": {
         const userData = {
-          email: data.email_addresses[0].email_address,
-          name: data.first_name + " " + data.last_name,
+          email: data.email_addresses[0].email_address, // Fix: Correct field name
+          name: `${data.first_name} ${data.last_name}`,
           imageUrl: data.image_url,
         };
         await User.findByIdAndUpdate(data.id, userData);
-        res.json({});
+        res.json({ success: true });
         break;
       }
       case "user.deleted": {
         await User.findByIdAndDelete(data.id);
-        res.json({});
+        res.json({ success: true });
         break;
       }
-
       default:
-        break;
+        res
+          .status(400)
+          .json({ success: false, message: "Unhandled event type" });
     }
   } catch (error) {
-    res.json({ success: false, message: error.message });
+    res.status(500).json({ success: false, message: error.message });
   }
 };
